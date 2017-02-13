3f8356c221309af1b44b4c6a2981fc7df89065e9
Rizki Aprilian <Rizki Aprilian>
2017-02-13 11:08:38 +0700

    mailgun

---
+++ fuel/app/logs/2017/02/13.php
@@ -0,0 +1,46 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+ERROR - 2017-02-13 02:40:10 --> Error - Class 'Mailgun\Mailgun' not found in C:\xampp7\htdocs\kubeds\fuel\packages\email\classes\email\driver\mailgun.php on line 25
+ERROR - 2017-02-13 02:44:59 --> Error - Your credentials are incorrect. in C:\xampp7\htdocs\kubeds\fuel\vendor\mailgun\mailgun-php\src\Mailgun\Connection\RestClient.php on line 228
+ERROR - 2017-02-13 02:49:35 --> Notice - Undefined variable: url in C:\xampp7\htdocs\kubeds\fuel\app\views\email\create_password.php on line 5
+ERROR - 2017-02-13 03:04:35 --> Error - Call to a member function to_array() on array in C:\xampp7\htdocs\kubeds\fuel\app\classes\controller\password.php on line 15
+ERROR - 2017-02-13 03:05:28 --> Error - Call to a member function as_array() on array in C:\xampp7\htdocs\kubeds\fuel\app\classes\controller\password.php on line 16
+ERROR - 2017-02-13 03:05:41 --> Notice - Undefined index: email in C:\xampp7\htdocs\kubeds\fuel\app\classes\controller\password.php on line 16
+ERROR - 2017-02-13 03:05:46 --> Notice - Trying to get property of non-object in C:\xampp7\htdocs\kubeds\fuel\app\classes\controller\password.php on line 16
+ERROR - 2017-02-13 03:06:32 --> Error - Call to a member function to_array() on array in C:\xampp7\htdocs\kubeds\fuel\app\classes\controller\password.php on line 10
+ERROR - 2017-02-13 03:07:56 --> Warning - Creating default object from empty value in C:\xampp7\htdocs\kubeds\fuel\app\classes\controller\password.php on line 16
+ERROR - 2017-02-13 03:09:54 --> Warning - Attempt to assign property of non-object in C:\xampp7\htdocs\kubeds\fuel\app\classes\controller\password.php on line 17
+ERROR - 2017-02-13 03:10:20 --> Warning - Attempt to assign property of non-object in C:\xampp7\htdocs\kubeds\fuel\app\classes\controller\password.php on line 13
+ERROR - 2017-02-13 03:11:25 --> Notice - Undefined variable: styles in C:\xampp7\htdocs\kubeds\fuel\app\views\base_page.php on line 9
+ERROR - 2017-02-13 03:11:31 --> Notice - Undefined variable: logged_in in C:\xampp7\htdocs\kubeds\fuel\app\views\base_page.php on line 28
+ERROR - 2017-02-13 03:14:10 --> Warning - Attempt to assign property of non-object in C:\xampp7\htdocs\kubeds\fuel\app\classes\controller\password.php on line 13
+ERROR - 2017-02-13 03:15:26 --> Error - Call to a member function to_array() on null in C:\xampp7\htdocs\kubeds\fuel\app\classes\controller\password.php on line 39
+ERROR - 2017-02-13 03:16:49 --> Error - Call to a member function to_array() on null in C:\xampp7\htdocs\kubeds\fuel\app\classes\controller\password.php on line 39
+ERROR - 2017-02-13 03:20:44 --> Warning - Attempt to assign property of non-object in C:\xampp7\htdocs\kubeds\fuel\app\classes\controller\login.php on line 8
+ERROR - 2017-02-13 03:23:33 --> Warning - Attempt to assign property of non-object in C:\xampp7\htdocs\kubeds\fuel\app\classes\controller\login.php on line 8
+ERROR - 2017-02-13 03:25:24 --> Notice - Undefined variable: logged_in in C:\xampp7\htdocs\kubeds\fuel\app\views\base_page.php on line 28
+ERROR - 2017-02-13 03:26:54 --> Warning - Attempt to assign property of non-object in C:\xampp7\htdocs\kubeds\fuel\app\classes\controller\login.php on line 8
+ERROR - 2017-02-13 03:28:35 --> Warning - Attempt to assign property of non-object in C:\xampp7\htdocs\kubeds\fuel\app\classes\controller\login.php on line 8
+ERROR - 2017-02-13 03:29:29 --> Warning - Attempt to assign property of non-object in C:\xampp7\htdocs\kubeds\fuel\app\classes\controller\login.php on line 8
+ERROR - 2017-02-13 03:33:02 --> Warning - Creating default object from empty value in C:\xampp7\htdocs\kubeds\fuel\app\classes\controller\password.php on line 17
+ERROR - 2017-02-13 03:37:29 --> Notice - Undefined property: Authlite\Authlite::$token in C:\xampp7\htdocs\kubeds\fuel\app\classes\controller\auth.php on line 32
+ERROR - 2017-02-13 03:40:15 --> Notice - Undefined property: Authlite\Authlite::$token in C:\xampp7\htdocs\kubeds\fuel\app\classes\controller\auth.php on line 32
+ERROR - 2017-02-13 03:56:54 --> Notice - Trying to get property of non-object in C:\xampp7\htdocs\kubeds\fuel\app\classes\controller\auth.php on line 34
+ERROR - 2017-02-13 03:57:31 --> Notice - Trying to get property of non-object in C:\xampp7\htdocs\kubeds\fuel\app\classes\controller\auth.php on line 34
+ERROR - 2017-02-13 04:03:03 --> Warning - Attempt to assign property of non-object in C:\xampp7\htdocs\kubeds\fuel\app\classes\controller\password.php on line 15
+ERROR - 2017-02-13 04:04:25 --> Warning - Attempt to assign property of non-object in C:\xampp7\htdocs\kubeds\fuel\app\classes\controller\password.php on line 15
+ERROR - 2017-02-13 04:04:58 --> Warning - Attempt to assign property of non-object in C:\xampp7\htdocs\kubeds\fuel\app\classes\controller\password.php on line 15
+ERROR - 2017-02-13 04:06:40 --> Warning - Attempt to assign property of non-object in C:\xampp7\htdocs\kubeds\fuel\app\classes\controller\password.php on line 15
+ERROR - 2017-02-13 04:09:07 --> Warning - Attempt to assign property of non-object in C:\xampp7\htdocs\kubeds\fuel\app\classes\controller\password.php on line 15
+ERROR - 2017-02-13 04:10:01 --> Warning - Attempt to assign property of non-object in C:\xampp7\htdocs\kubeds\fuel\app\classes\controller\password.php on line 15
+ERROR - 2017-02-13 04:12:57 --> Warning - Attempt to assign property of non-object in C:\xampp7\htdocs\kubeds\fuel\app\classes\controller\password.php on line 15
+ERROR - 2017-02-13 04:31:10 --> Error - View variable is not set: data in C:\xampp7\htdocs\kubeds\fuel\core\classes\view.php on line 539
+ERROR - 2017-02-13 04:31:32 --> Warning - Creating default object from empty value in C:\xampp7\htdocs\kubeds\fuel\app\classes\controller\password.php on line 11
+ERROR - 2017-02-13 04:32:13 --> Warning - Creating default object from empty value in C:\xampp7\htdocs\kubeds\fuel\app\classes\controller\password.php on line 11
+ERROR - 2017-02-13 04:32:17 --> Warning - Creating default object from empty value in C:\xampp7\htdocs\kubeds\fuel\app\classes\controller\password.php on line 11
+ERROR - 2017-02-13 04:32:21 --> Warning - Creating default object from empty value in C:\xampp7\htdocs\kubeds\fuel\app\classes\controller\password.php on line 12
+ERROR - 2017-02-13 04:33:49 --> Warning - Creating default object from empty value in C:\xampp7\htdocs\kubeds\fuel\app\classes\controller\password.php on line 16
+ERROR - 2017-02-13 04:34:27 --> Warning - Creating default object from empty value in C:\xampp7\htdocs\kubeds\fuel\app\classes\controller\password.php on line 10
+ERROR - 2017-02-13 04:35:08 --> Notice - Undefined variable: meta_title in C:\xampp7\htdocs\kubeds\fuel\app\views\base_page.php on line 6
+ERROR - 2017-02-13 04:35:24 --> Notice - Undefined variable: styles in C:\xampp7\htdocs\kubeds\fuel\app\views\base_page.php on line 9
+ERROR - 2017-02-13 04:35:57 --> Notice - Undefined property: Controller\Password::$auto_render in C:\xampp7\htdocs\kubeds\fuel\app\classes\controller\password.php on line 10
