c48105b3ae9ace9bf55f14889c7c2bb8f42fa710
Rizki Aprilian <Rizki Aprilian>
2017-03-11 18:18:16 +0700

    custom datatable style

---
+++ fuel/app/logs/2017/03/11.php
@@ -0,0 +1,3 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+ERROR - 2017-03-11 12:15:56 --> Error - Could not find asset: styles.css in C:\xampp7\htdocs\kubeds\fuel\core\classes\asset\instance.php on line 413
--- fuel/app/views/base_page.php
+++ fuel/app/views/base_page.php
@@ -5,8 +5,8 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<title><?=$meta_title;?></title>
 		<link href="/assets/css/app.css" rel="stylesheet" type="text/css" media="screen" />
-		<link href="/assets/css/style.css" rel="stylesheet" type="text/css" media="screen" />
 		<?= \Asset::css($styles, array(), null, false); ?>
+		<link href="/assets/css/style.css" rel="stylesheet" type="text/css" media="screen" />
 		<script src="//code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
 		<script src="/assets/js/foundation.min.js" type="text/javascript"></script>
 		<?= \Asset::js($scripts, array(), null, false); ?>
