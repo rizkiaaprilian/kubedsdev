ce9bf324e45f8ce40b5f09b225b6a7ddf8f1d8fa
Rizki Aprilian <rizki.a.aprilian@gmail.com>
2017-06-30 15:56:38 +0700

    Update db.php

--- fuel/app/config/production/db.php
+++ fuel/app/config/production/db.php
@@ -6,7 +6,7 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=us-cdbr-iron-east-03.cleardb.net;dbname=heroku_4c6eaa01dc3d95c	',
+			'dsn'        => 'mysql:host=us-cdbr-iron-east-03.cleardb.net;dbname=heroku_4c6eaa01dc3d95c',
 			'username'   => 'ba82694606d3bf',
 			'password'   => 'da1fdd24',
 		),
